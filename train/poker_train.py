92138082a606e6d96bc15596a6d67989509bf301
kabazoka <73285479+kabazoka@users.noreply.github.com>
2023-10-25 20:09:16 +0800

    update readme

--- train/poker_train.py
+++ train/poker_train.py
@@ -1,8 +1,2 @@
-# yolov8n.pt file 
-from google.colab import drive
-drive.mount('/content/drive') # here I simply stored my file in google drive
-!pip install ultralytics
-from  ultralytics import YOLO
-!yolo task=detect mode=train model=yolov8n.pt data='Your dataset path in google drive' epochs=50 imgsz=640
-# Results will be saved to runs/detect/predict in drive
-# you need to download that pre-trained prediction file to work with.
+### Download dataset from https://universe.roboflow.com/roboflow-100/poker-cards-cxcvz
+!yolo task=detect mode=train model=yolov8n.pt data='<dataset_path>' epochs=50 imgsz=640
